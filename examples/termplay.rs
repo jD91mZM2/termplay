edba83177abd7d9a233992e96f969e86a7c26906
jD91mZM2 <me@krake.one>
2018-04-08 16:32:05 +0200

    Add --converter option

--- examples/termplay.rs
+++ examples/termplay.rs
@@ -7,7 +7,7 @@ extern crate termplay;
 
 use clap::{Arg, App};
 use failure::Error;
-use image::{FilterType, GenericImage};
+use image::{FilterType, GenericImage, Pixel};
 use std::{
     cell::RefCell,
     io::{self, Write},
@@ -21,26 +21,10 @@ use termion::{
     screen::AlternateScreen
 };
 use termplay::{
-    converters::{Converter, TrueColor},
+    converters::*,
     resizer,
     Zoomer
 };
-//use ears::Music;
-//use image::{imageops, DynamicImage, GenericImage, FilterType};
-//use std::{
-//    cmp::min,
-//    io::{self, BufWriter, Write},
-//    sync::{Arc, Mutex},
-//    thread,
-//    time::Duration
-//};
-//use termplay::{
-//    Playback,
-//    MusicPlayback,
-//    converters::{Converter, TrueColor},
-//    resizer,
-//    zoomer::Zoomer
-//};
 
 pub struct Hide<W: Write>(W);
 impl<W: Write> From<W> for Hide<W> {
@@ -60,6 +44,23 @@ impl<W: Write> Write for Hide<W> {
     }
     fn flush(&mut self) -> io::Result<()> {
         self.0.flush()
+    }
+}
+
+pub enum ConverterType {
+    TrueColor,
+    Color256
+}
+impl Converter for ConverterType {
+    fn display<W, I, P>(&self, fmt: &mut W, image: &I) -> Result<(), io::Error>
+        where W: Write,
+              I: GenericImage<Pixel = P>,
+              P: Pixel<Subpixel = u8>
+    {
+        match *self {
+            ConverterType::TrueColor => TrueColor.display(fmt, image),
+            ConverterType::Color256 => Color256.display(fmt, image)
+        }
     }
 }
 
@@ -98,9 +99,22 @@ fn do_main() -> Result<(), Error> {
                 .help("Sets the terminal font ratio (only takes effect with some converters)")
                 .long("ratio")
                 .takes_value(true))
+            .arg(Arg::with_name("converter")
+                .help("Decides how the image should be displayed")
+                .short("c")
+                .long("converter")
+                .possible_values(&["truecolor", "color256"])
+                .default_value("truecolor")
+                .takes_value(true))
             .get_matches();
 
     let path = options.value_of("path").unwrap();
+
+    let converter = match options.value_of("converter").unwrap() {
+        "truecolor" => ConverterType::TrueColor,
+        "color256"  => ConverterType::Color256,
+        _ => unreachable!()
+    };
 
     let (mut width, mut height) = termion::terminal_size().map(|(w, h)| (w as u32, h as u32)).unwrap_or((80, 24));
     if let Ok(w) = value_t!(options, "width", u32) {
@@ -123,7 +137,7 @@ fn do_main() -> Result<(), Error> {
             height = h;
         }
         image = image.resize_exact(width, height, FilterType::Nearest);
-        TrueColor.display(&mut stdout, &image)?;
+        converter.display(&mut stdout, &image)?;
         return Ok(());
     }
 
@@ -148,7 +162,7 @@ fn do_main() -> Result<(), Error> {
         image = image.resize_exact(width, height, FilterType::Nearest);
 
         write!(stdout, "{}", cursor::Goto(1, 1))?;
-        TrueColor.display(&mut stdout, &image)?;
+        converter.display(&mut stdout, &image)?;
         Ok(())
     };
     draw()?;
@@ -185,110 +199,3 @@ fn do_main() -> Result<(), Error> {
 
     Ok(())
 }
-
-/*
-    let player = Playback::new(30);
-    let music = Music::new("test.wav").unwrap();
-    let player = Arc::new(Mutex::new(MusicPlayback::new(player, music)));
-    let player2 = Arc::clone(&player);
-    let player3 = Arc::clone(&player);
-
-    let zoomer = Arc::new(Mutex::new(Zoomer::new()));
-    let zoomer2 = Arc::clone(&zoomer);
-
-    thread::spawn(move || {
-        push(&player2);
-    });
-    thread::spawn(move || -> Result<(), io::Error> {
-        let stdin = io::stdin();
-        for event in stdin.events() {
-            match event? {
-                Event::Key(Key::Char(' ')) => {
-                    let mut player = player3.lock().unwrap();
-                    if player.is_paused() {
-                        player.play();
-                    } else {
-                        player.pause();
-                    }
-                },
-                Event::Key(Key::Ctrl('c')) => {
-                    let mut player = player3.lock().unwrap();
-                    player.stop();
-                },
-                Event::Mouse(MouseEvent::Hold(x, y)) => {
-                    let mut zoomer = zoomer2.lock().unwrap();
-                    zoomer.set_pos(x, y);
-                    let mut player = player3.lock().unwrap();
-                    player.redraw();
-                },
-                Event::Mouse(MouseEvent::Press(btn, x, y)) => {
-                    let mut zoomer = zoomer2.lock().unwrap();
-                    let level = zoomer.level();
-                    zoomer.set_pos(x, y);
-                    match btn {
-                        MouseButton::WheelUp => zoomer.set_level(level.saturating_sub(5)),
-                        MouseButton::WheelDown => zoomer.set_level(level + 5),
-                        _ => ()
-                    }
-                    let mut player = player3.lock().unwrap();
-                    player.redraw();
-                },
-                _ => ()
-            }
-        }
-        Ok(())
-    });
-
-    println!("Let the processing get ahead");
-    thread::sleep(Duration::from_secs(3));
-    println!("Go go go!");
-
-    let stdout = io::stdout();
-    stdout.lock();
-    let stdout = match stdout.into_raw_mode() {
-        Ok(stdout) => stdout,
-        Err(err) => {
-            eprintln!("error: {}", err);
-            return;
-        }
-    };
-    let stdout = MouseTerminal::from(stdout);
-    let mut stdout = BufWriter::new(stdout);
-
-    writeln!(stdout, "\x1b[?1049h\x1b[?25l").unwrap();
-    MusicPlayback::run(|| player.lock().unwrap(), |frame| {
-        if let Some(mut frame) = frame {
-            write!(stdout, "\x1b[;H").unwrap(); stdout.flush().unwrap();
-            let (width, height) = resizer::keep_aspect_ratio(frame.width(), frame.height(), 167-4, 40-4);
-            let (width, height) = resizer::with_font_ratio(width, height, 45);
-
-            let zoomer = zoomer.lock().unwrap();
-            let frame = zoomer.crop(&mut frame, width, height);
-            let frame = imageops::resize(&frame, width, height, FilterType::Nearest);
-            TrueColor.display(&mut stdout, &frame).unwrap();
-        } else {
-            //let mut player = player.lock().unwrap();
-            //player.pause();
-            writeln!(stdout, "\x1b[;H\x1b[JHold on, you're going too fast! Waiting...").unwrap();
-            stdout.flush().unwrap();
-            thread::sleep(Duration::from_secs(1));
-            //player.play();
-        }
-    });
-    writeln!(stdout, "\x1b[?25h\x1b[?1049h").unwrap();
-}
-
-fn push(player: &Mutex<MusicPlayback<DynamicImage>>) {
-    let frames = 9682;
-
-    let mut i = 1;
-    while i < frames {
-        let mut buf = Vec::with_capacity(50);
-        for _ in 0..min(frames-i, 50) {
-            buf.push(image::open(format!("test/frame{}.png", i)).unwrap());
-            i += 1;
-        }
-        player.lock().unwrap().extend(buf);
-    }
-}
-*/
